6bc80ae224f444be51aa65e9fc77682b97228585
andrewklundtFT <ak1036178@student.francistuttle.edu>
2023-09-07 15:02:31 -0500

    finished

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
      public static void main(String[] args) throws Exception {
-        //Student a = new Student();
+        Student a = new Student("andrew", "klundt", 4.0, "2300");
+        Student b = new Student("grayson", "smallwood", 2.0, "2301");
+        GradStudent c = new GradStudent("abraham", "khan", 4.0,"2302", 1, "1");
+        GradStudent d = new GradStudent("matthew", "maddox", 4.0,"2303", 2, "1");
+        a.setFirstName("drewski");
+        b.setGPA(1.6);
+        List<Student> studentList = new ArrayList();
+        studentList.add(a);
+        studentList.add(b);
+        studentList.add(c);
+        studentList.add(d);
+        for (Student k : studentList) {
+            System.out.println(k);
+        }
      }
 }
